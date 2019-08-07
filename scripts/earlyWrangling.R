6fdda466ad2bd07cc5305fd827c7d81271cc4f51
panibiralo <khadka.bijit17@gmail.com>
2019-08-07 11:00:32 -0400

    createFile

---
+++ scripts/earlyWrangling.R
@@ -0,0 +1,14 @@
+## bringing the data in and cleaning ##
+
+# setup -------------------------------------------------------------------
+##load libraries:
+
+library(tidyverse)
+
+##funtions
+source("scripts/source_file.R")
+
+
+# Wrangle -----------------------------------------------------------------
+
+
---
+++ scripts/source_file.R
@@ -0,0 +1,8 @@
+#### First file to create: source file
+#### this script will contain functions that I dont want to see.
+
+
+addOne <- 
+  function(x){
+    x+1
+  }
